75023dba38c401b3f27636372cdd409371da81c8
Emma Finkel <emma.finkel@gmail.com>
2020-04-27 17:37:37 -0400

    Initial Commit

---
+++ To-Do List Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-Do List Project
+//
+//  Created by Emma Finkel on 4/27/20.
+//  Copyright © 2020 Emma Finkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
